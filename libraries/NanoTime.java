8b31b7045127e917eb5b7ce0046e07b94ff25bb4
Kuan-Yu Chen <starmap0312@gmail.com>
2017-02-03 09:48:28 +0800

    add more

---
+++ libraries/NanoTime.java
@@ -0,0 +1,9 @@
+public class NanoTime {
+
+    public static void main(String[] args) {
+        // 1) System.currentTimeMillis(): elapsed time in milliseconds since the epoch
+        System.out.println(System.currentTimeMillis());
+        // 2) System.nanoTime(): elapsed time in nanoseconds since some fixed but arbitrary time (may be negative)
+        System.out.println(System.nanoTime());
+    }
+}
