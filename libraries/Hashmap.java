a30842f43e8bb16401ab776f59bc355f458cd13c
Kuan-Yu Chen <starmap0312@gmail.com>
2017-02-03 14:19:44 +0800

    add more

--- libraries/Hashmap.java
+++ libraries/Hashmap.java
@@ -1,10 +1,20 @@
 import java.util.Map;
+import java.util.HashSet;
 import java.util.HashMap;
 import java.util.Iterator;
 
 public class Hashmap {
 
     public static void main(String[] args) {
+        HashSet<String> s = new HashSet<String>();
+        s.add("d");
+        s.add("c");
+        s.add("a");
+        Iterator it = s.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
         HashMap<Integer, String> mp = new HashMap<Integer, String>();
         // put(key, value): insert key, value pair into the hashmap
         mp.put(3, "three");
