377c4273337248142bb58ad9c304da779916a9a8
Kuan-Yu Chen <starmap0312@gmail.com>
2017-02-03 14:01:35 +0800

    add more

---
+++ libraries/AppTest.java
@@ -0,0 +1,10 @@
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+public class AppTest extends TestCase {
+    public void testApp() {
+        // fail("this will throw AssertionFailedError");
+        assertTrue(true);
+        assertEquals("check two strings", "abc", "abc"); 
+    }
+}
