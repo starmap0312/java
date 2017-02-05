fadcd98e24f6ba58e9a9b4f97d9b4dba1eb265b4
Kuan-Yu Chen <starmap0312@gmail.com>
2017-02-05 21:03:15 +0800

    add more

--- libraries/Strings.java
+++ libraries/Strings.java
@@ -1,25 +1,36 @@
+// String object are immutalbe
+
 public class Strings {
 
     public static void main(String[] args) {
-        String str1 = new String("abcde");  // this creates an immutable String object in heap
-        String str2 = new String("abcde");
-        // 1.0) length():
+        String str1 = new String("abcde");         // this creates an immutable String object in heap
+        // 1.0) length(): return the length of a string
         System.out.println(str1.length());
-        // 1.1) indexOf(substr, [pos]):
+        // 1.1) indexOf(substr, [pos]): return the start position of a substring
         System.out.println(str1.indexOf("c"));
-        // 1.2) substring(start, end):
-        System.out.println(str1.substring(2, 4));
+        // 1.2) substring(start, [end]): return the substring given the start position [and end position]
+        String str3 = new String("01234");
+        System.out.println(str3.substring(2));
+        System.out.println(str3.substring(2, 4));
         // 1.3) concat() or +: create a new String object that concats two strings
-        String str3 = str1.concat(str2) + "123"; 
-        System.out.println(str3); 
+        String str4 = str1.concat("fgh") + "ijk";  // this creates a new immutable String object
+        System.out.println(str4); 
         // 1.4) charAt(): reference indivisual character, note that str1[3] is WRONG
         System.out.println(str1.charAt(2));
         // 2) str1 == str2 vs. str1.equals(str2)
+        String str1_copy = str1;                   // two separate references refer to the same String object
+        if (str1 == str1_copy) {
+            System.out.println("str1 & str1_copy refer to the same object");
+        }
+        if (str1.equals(str1_copy)) {
+            System.out.println("str1 & str1_copy refer to the same object and have the same content");
+        }
+        String str2 = new String("abcde");         // a new immutable String object
         if (str1 == str2) {
-            System.out.println("str1 & str2 are two identical references");
+            System.out.println("str1 & str2 refer to the same object");
         }
         if (str1.equals(str2)) {
-            System.out.println("str1 & str2 store identical characters");
+            System.out.println("str1 & str2 refer to different objects but have the same content");
         }
         // 3) toCharArray(): creates a copy of array object of char
         char cArray[] = str1.toCharArray(); // this creates an array object of char in heap 
@@ -29,14 +40,19 @@ public class Strings {
                 cArray[i] = 'x';
             }
         }
-        String str4 = new String(cArray);
-        for (char c : str4.toCharArray()) { // for-each syntax for iterating an array object
+        String str5 = new String(cArray);   // initialize String object with an char[] array
+        for (char c : str5.toCharArray()) { // for-each syntax for iterating an array object
             System.out.println(c);
         } 
         // 5) split(String regex): split a string object based on a passed-in regular expression
-        String words[] = str3.split("c|1");
+        String words[] = str5.split("c|1");
         for (String s : words) {
             System.out.println(s);
         }
+        // 6) StringBuilder: Java mutable String object
+        StringBuilder sb = new StringBuilder("mutable");
+        sb.append("string");
+        sb.insert(7, " ");
+        System.out.println(sb);
     }
 }
