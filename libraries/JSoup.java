3d09006665cf12237acccbd3027abefded4104b6
kuanyu <starmap0312@gmail.com>
2017-02-16 09:45:44 +0800

    add more

--- libraries/JSoup.java
+++ libraries/JSoup.java
@@ -1,5 +1,6 @@
 // jsoup: Java HTML Parser
-// 1) it provides a very convenient API for extracting and manipulating data, using DOM, CSS, and jquery-like methods
+// 1) it provides a very convenient API for extracting and manipulating data
+//    it uses DOM, CSS, and jquery-like methods
 // 2) it parses HTML to the same DOM as modern browsers do
 //    a) scrape and parse HTML from a URL, file, or string
 //    b) find and extract data, using DOM traversal or CSS selectors
@@ -14,7 +15,7 @@ import java.io.IOException;
 public class JSoup {
 
     public static void main(String[] args) {
-        // 1) parse HTML from a string
+        // 1) parse(): parse HTML from a string
         String html = "<html>"
                     + "<head><title>Title</title></head>"
 		    + "<body><h1>Body Content</h1></body>"
@@ -23,7 +24,7 @@ public class JSoup {
         System.out.println(doc.title());
         System.out.println(doc.head());
         System.out.println(doc.body());
-        // 2) parse HTML from a URL 
+        // 2) parse(): parse HTML from a URL 
         try {
             Document doc2 = Jsoup.connect("http://en.wikipedia.org/").get();
             System.out.println(doc2.title());
