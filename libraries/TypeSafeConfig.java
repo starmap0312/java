450636acef682387dc68f2a0e9b9734b8db303eb
kuanyu <starmap0312@gmail.com>
2017-02-16 10:39:26 +0800

    add more

--- libraries/TypeSafeConfig.java
+++ libraries/TypeSafeConfig.java
@@ -7,8 +7,17 @@ public class TypeSafeConfig {
 
     public static void main(String[] args) {
         Config config = ConfigFactory.load("example.conf"); 
+        // example 1: my.organization object
         System.out.println(config.getString("my.organization.project.description"));
         System.out.println(config.getInt("my.organization.team.age"));
         System.out.println(config.getStringList("my.organization.team.members"));
+        // example 2: foo object 
+        System.out.println(config.getInt("foo.key.one"));
+        System.out.println(config.getInt("dup.key.one"));
+        System.out.println(config.getInt("dup.key.two"));
+        // example 3: array objects
+        System.out.println(config.getIntList("array1"));
+        System.out.println(config.getIntList("array2"));
+        System.out.println(config.getIntList("array3"));
     }
 }
