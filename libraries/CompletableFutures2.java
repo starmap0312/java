4ee05f535c40bee5dcd97ea7377e036ec68bd9da
kuanyu <starmap0312@gmail.com>
2017-02-23 11:30:26 +0800

    add more

--- libraries/CompletableFutures2.java
+++ libraries/CompletableFutures2.java
@@ -10,22 +10,41 @@ public class CompletableFutures2 {
         //    returns: a new CompletionStage that is executed with previous stage's result as the argument to the supplied function
         // 2) thenAccept([function]):
         //    returns a new CompletionStage that is executed with previous stage's result as the argument to the supplied function 
+        System.out.println("Example 1:");
         CompletableFuture future = CompletableFuture.supplyAsync(
             () -> {
                 System.out.println("Do supplyAsync task");
-                return "Value";
+                return "Step 1 return";
             }
         ).thenApply(
             (String result) -> {
                 System.out.println("Got supplyAsync's return: " + result);
                 System.out.println("Do thenApply task");
-                return "thenApply is complete";
+                return "Step 2 return";
             }
         ).thenAccept(
             (String result) -> {
                 System.out.println("Got thenApply's return: " + result);
                 System.out.println("Do thenAccept task");
             }
+        );
+
+        // 3) thenCompose([function]):
+        //    returns: a new CompletionStage that is executed with this stage as the argument to the supplied function
+        System.out.println("Example 2:");
+        CompletableFuture future2 = CompletableFuture.supplyAsync(
+            () -> {
+                System.out.println("Do supplyAsync task");
+                return "Step 1 return";
+            }
+        ).thenCompose(
+            (String result) -> CompletableFuture.supplyAsync(
+                () -> {
+                    System.out.println("Got supplyAsync's return: " + result);
+                    System.out.println("Do thenCompose task");
+                    return "Step 2 return";
+                }
+            )
         );
     }
 }
