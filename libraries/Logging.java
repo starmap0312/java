8efd4e91ab77de78a0aa8efee644a01518004b4c
kuanyu <starmap0312@gmail.com>
2017-03-09 11:06:19 +0800

    add more

--- libraries/Logging.java
+++ libraries/Logging.java
@@ -5,15 +5,17 @@ import org.slf4j.LoggerFactory;
 
 public class Logging {
     // LoggerFactory.getLogger([classname])
-    //private Logger logger = LoggerFactory.getLogger(getClass());
-    // note: non-static method getClass() cannot be referenced from static context 
-    //private static Logger logger = LoggerFactory.getLogger("com.domain.mypackage");
-    private static Logger logger = LoggerFactory.getLogger(Logging.class);
+    private static Logger logger = LoggerFactory.getLogger(Logging.class.getName());
+    private static Logger rootLogger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
 
     public void writeLog() {
+        System.out.println(Logging.class.getName());
         logger.error("an error message");      // [main] ERROR Logging - message
         logger.warn("a warning message");      // [main] WARN Logging - message
         logger.debug("a debug message");       // [main] INFO Logging - message
+        rootLogger.error("an error message");      // [main] ERROR Logging - message
+        rootLogger.warn("a warning message");      // [main] WARN Logging - message
+        rootLogger.debug("a debug message");       // [main] INFO Logging - message
     }
 
     public static void main(String[] args) {
